87c6d930e889b1d2030fbfa2fd61712931015bae
dongorae <ldjnewdress@gmail.com>
2023-08-15 18:19:59 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function redirectQualifyingOrientation() {
+    var password = prompt("비밀번호를 입력하세요.");
+    var correctPassword = "230917";
+    if (password === correctPassword) {
+        window.open("https://docs.google.com/presentation/d/1hIRGknnjRp-wKz83Ur6mSAYU5LsmeYiDjs5conLcv7s/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+} 
+
+function redirectQualifyingRules() {
+    var password = prompt("비밀번호를 입력하세요.");
+    var correctPassword = "230917";
+    if (password === correctPassword) {
+        window.open("https://docs.google.com/document/d/1Up-aQqbMlxHGBL0e8ZG6sdbBp9oVdyeKGDcdp_eZr1w/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+}
+
+
+function redirectFinalsOrientation() {
+    var password = prompt("비밀번호를 입력하세요.");
+    var correctPassword = "230917";
+    if (password === correctPassword) {
+        window.open("https://docs.google.com/presentation/d/1elIDIQhPtHEYoZPegkI-ZTywz1nDuYJHlKIh7TEZ0cg/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+}
+
+
+function redirectFinalsRules() {
+    var password = prompt("비밀번호를 입력하세요.");
+    var correctPassword = "230917";
+    if (password === correctPassword) {
+        window.open("https://docs.google.com/document/d/1fA7hRFDJTz2y2NqR-GqLIlHIFDAYvEH9Lpm2FEAu7Dw/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+} 
