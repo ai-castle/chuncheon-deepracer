1d0a77959592401bca47c0d85c1765ceb19025a3
dongorae <ldjnewdress@gmail.com>
2024-05-28 13:46:02 +0900

    m

---
+++ 2024-temp/assets/js/script.js
@@ -0,0 +1,98 @@
+var 예선Password = "abcd2024";
+var 본선Password = "gogo2024";
+
+//////////////// 공통 자료 ///////////////
+function 대회일정표() {
+    window.open("/2024/assets/img/대회일정표.svg", "_blank");
+}
+
+function 참가자접수() {
+    window.open("https://forms.gle/tw6VGkGgnWobpw8s8", "_blank");
+} 
+
+function 계정사용량() {
+    window.open("/2024/no-page.html", "_blank");
+}
+
+//////////////// 예선 관련 자료 //////////
+function 예선오픈채팅() {
+    window.open("https://open.kakao.com/o/g31Mylqg", "_blank");
+}
+
+function 예선OT() {
+        window.open("/2024/no-page.html", "_blank");
+}
+
+function 예선OT자료() {
+    var password = prompt("비밀번호(예선)를 입력하세요.");
+    if (password === 예선Password) {
+        window.open("https://docs.google.com/presentation/d/1wBaggna4WoIjmXs6JlYWk_ezveySsRKI4FRHrfWbNTU/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+} 
+
+function 예선규칙() {
+    var password = prompt("비밀번호(예선)를 입력하세요.");
+    if (password === 예선Password) {
+        window.open("https://docs.google.com/document/d/1867mdov5puu9P_i2bRVbg1-_jRn_T8N5oRpO6nLOugE/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+}
+
+function 예선랭킹() {
+    window.open("/2024/no-page.html", "_blank");
+}
+
+
+////////////// 본선 관련 링크 //////////////
+function 본선오픈채팅() {
+    window.open("https://open.kakao.com/o/guJ1zlqg", "_blank");
+}
+
+
+function 본선OT() {
+        window.open("/2024/no-page.html", "_blank");
+}
+
+function 본선OT자료() {
+    var password = prompt("비밀번호(본선)를 입력하세요.");
+    if (password === 본선Password) {
+        window.open("https://docs.google.com/presentation/d/1JVUoHKParptDxRPQFUoHL0_Vjzyb52vG-wNg0EZyIQ4/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+}
+
+
+function 본선규칙() {
+    var password = prompt("비밀번호(본선)를 입력하세요.");
+    if (password === 본선Password) {
+        window.open("https://docs.google.com/document/d/1ucv70XA-FjQ2wSGsXGG6ECdz5H1nMbADqwOU3yqrj3Q/edit?usp=sharing", "_blank");
+    } else {
+        alert("비밀번호가 잘못되었습니다.");
+    }
+} 
+
+function 본선랭킹() {
+    window.open("/2024/no-page.html", "_blank");
+}
+
+function 본선테스트주행시간표() {
+    window.open("/2024/assets/img/본선테스트주행시간표.jpg", "_blank");
+}
+
+function 본선숙소위치() {
+    window.open("https://naver.me/xUEItkPy", "_blank");
+}
+
+function 본선시간표() {
+    window.open("/2024/assets/img/본선시간표.jpg", "_blank");
+}
+
+function 본선라이브스트리밍() {
+    window.open("/2024/no-page.html", "_blank");
+}
+
+
